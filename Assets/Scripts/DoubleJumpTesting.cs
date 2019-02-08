4bf234e8715a1021559cd0a8e2c04642988a3a74
brandonpayne55 <brandon.payne@ufl.edu>
2019-02-08 08:47:10 -0500

    stuff

--- Assets/Scripts/DoubleJumpTesting.cs
+++ Assets/Scripts/DoubleJumpTesting.cs
@@ -8,11 +8,17 @@ public class DoubleJumpTesting : MonoBehaviour
     public AudioSource coinAudioSource;
     public float walkSpeed = 8f;
     public float jumpSpeed = 10f;
+
     bool touchingWallLeft = false;
     bool touchingWallRight = false;
     bool jumpedRight = false;
     bool jumpedLeft = false;
-    float wallTouchRadius = 0.3f;
+    float wallTouchRadius = 0.6f;
+    public GameObject MovingPlatform;
+    public Rigidbody MovPlat;
+    float thrust = 20f;
+
+
 
     // access the HUD
     public HudManager hud;
@@ -29,6 +35,10 @@ public class DoubleJumpTesting : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+
+        //rigid body of moving platform. drag and drop in unity
+        MovPlat = MovPlat.GetComponent<Rigidbody>();
+
         //get the rigid body component for later use
         rb = GetComponent<Rigidbody>();
 
@@ -40,11 +50,38 @@ public class DoubleJumpTesting : MonoBehaviour
     }
 
 
-
-
     // Update is called once per frame
     void Update()
     {
+        //checks if player is on top of trampoline
+
+        float sizeX = coll.bounds.size.x;
+        float sizeZ = coll.bounds.size.z;
+        float sizeY = coll.bounds.size.y;
+
+        Vector3 corner1 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.1f, sizeZ / 2);
+
+        //mask is used for the raycast. You can make your own layers, i just used default layers
+        LayerMask trampolineMask = LayerMask.GetMask("UI");
+        if (Physics.Raycast(corner1, new Vector3(0, -1, 0), 0.3f, trampolineMask))
+        {
+            /*
+            float temp = rb.velocity.y / 4;
+
+            if (temp < 0f)
+            {
+                temp = temp * (-1);
+            }
+            if (temp > 10f)
+            {
+                temp = 10f;
+            }
+            thrust += temp;
+            */
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        }
+
         // Handle player walking
         WalkHandler();
 
@@ -54,11 +91,34 @@ public class DoubleJumpTesting : MonoBehaviour
 
     }
 
+   
+
+
+
     // Make the player walk according to user input
     void WalkHandler()
     {
-        // Set x and z velocities to zero
-        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        //this first section checks to see if the player is on the moving platform
+        float sizeX = coll.bounds.size.x;
+        float sizeZ = coll.bounds.size.z;
+        float sizeY = coll.bounds.size.y;
+
+        Vector3 corner1 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.1f, sizeZ / 2);
+
+        //mask is used for the raycast. You can make your own layers, i just used default layers
+        LayerMask movingPlatformMask = LayerMask.GetMask("Water");
+
+        if (Physics.Raycast(corner1, new Vector3(0, -1, 0), 0.5f, movingPlatformMask))
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+            rb.velocity += MovPlat.velocity;
+        }
+
+        else
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
+
 
         // Distance ( speed = distance / time --> distance = speed * time)
         float distance = walkSpeed * Time.deltaTime;
@@ -91,16 +151,18 @@ public class DoubleJumpTesting : MonoBehaviour
         // Is grounded
         bool isGrounded = CheckGrounded();
 
-        // Is touching a wall
+
+        //Is touching a wall
+        //TODO add masks for the walls. check all directions of player, not just left and right
         touchingWallLeft = (Physics.Raycast(transform.position, Vector3.left, wallTouchRadius));
         touchingWallRight = (Physics.Raycast(transform.position, Vector3.right, wallTouchRadius));
-
 
 
         // Check if the player is pressing the jump key
         if (jAxis > 0f)
         {
             // Make sure we've not already jumped on this key press
+            //this makes sure that, if we are touching a wall, we didnt just jump from that wall
             if (!pressedJump && (isGrounded || (touchingWallLeft && !jumpedLeft) || (touchingWallRight && !jumpedRight)))
             {
                 // We are jumping on the current key press
@@ -162,6 +224,10 @@ public class DoubleJumpTesting : MonoBehaviour
         return (grounded1);
     }
 
+
+
+
+    /*
     void OnTriggerEnter(Collider collider)
     {
         // Check if we ran into a coin
@@ -196,5 +262,6 @@ public class DoubleJumpTesting : MonoBehaviour
             GameManager.instance.IncreaseLevel();
         }
 
-    }
+
+    }*/
 }
---
+++ Assets/Scripts/MovingPlatTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatTest : MonoBehaviour
+{
+    public Rigidbody q;
+    public Rigidbody player;
+    public GameObject pl;
+    int i = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        q = GetComponent<Rigidbody>();
+        player = player.GetComponent<Rigidbody>();
+        InvokeRepeating("ChangeSpeed", 1f, 5f);
+    }
+
+    void ChangeSpeed()
+    {
+        Vector3 mov;
+        if (i % 2 == 0)
+        {
+            mov = new Vector3(0f, 0f, 2f);
+        }
+        else
+        {
+            mov = new Vector3(0f, 0f, -2f);
+
+        }
+        i++;
+        q.velocity = mov;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //same as the one in the player controller. if player is on it, change velocity
+        if (Physics.Raycast(transform.position, new Vector3(0, 1, 0), 0.5f))
+        {
+            player.velocity = new Vector3(0f, player.velocity.y, 0f);
+            player.velocity += q.velocity;
+        }
+
+
+    }
+}
